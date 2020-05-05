245c346dd3ff5f7d960b93f6b63726180ea4c869
jael-VRR <jrodriguezruidias@gmail.com>
2020-05-05 02:52:40 -0500

    start library mk

---
+++ src/cli.js
@@ -0,0 +1,19 @@
+const mdLinks = require("./mdLinks.js");
+
+mdLinks("./some/example.md")
+  .then(links => {
+    // => [{ href, text, file }]
+  })
+  .catch(console.error);
+
+mdLinks("./some/example.md", { validate: true })
+  .then(links => {
+    // => [{ href, text, file, status, ok }]
+  })
+  .catch(console.error);
+
+mdLinks("./some/dir")
+  .then(links => {
+    // => [{ href, text, file }]
+  })
+  .catch(console.error);
---
+++ src/mdFile.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+console.log("File name:", __filename); 
+path1 = path.dirname(__filename); 
+console.log(path1); 
+   
+console.log("Directory name:", __dirname); 
+path2 = path.dirname(__dirname); 
+console.log(path2);
+const  mdLinks  =  require ( " mdLinks " );
+
